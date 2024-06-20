d4fd1f069705e041e51744d5c4327b9f0edd32d3
vladpyt <140207008+vladpyt@users.noreply.github.com>
2024-06-20 15:39:51 +0300

    First version of the site with image

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hi')
+    return HttpResponse("<img src='https://sdnem-rozhdeniya.ru/_ph/47/542611427.jpg'>")
