1f62e1f58c98d17af4009cf2991c6a988929b942
vladpyt <140207008+vladpyt@users.noreply.github.com>
2024-07-03 19:12:49 +0300

    First version of the site with image

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
-    products = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    products = Product.objects.all()
     return render(request, "index.html", {
         'products': products
     })
