a23af7223b26a72dc868ea8ad5b236b9686f5809
vladpyt <140207008+vladpyt@users.noreply.github.com>
2024-07-03 13:21:10 +0300

    First version of the site with image

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from django . db import models
 class Product(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=128)
     descript = models.TextField()
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
